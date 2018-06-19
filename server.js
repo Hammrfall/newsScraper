9cd8c333d530f8d104054d1785d338b20d396a43
Hammrfall <34660593+Hammrfall@users.noreply.github.com>
2018-06-19 15:50:14 -0400

    core functionality add

---
+++ server.js
@@ -0,0 +1,114 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+var exphbs = require("express-handlebars");
+var axios = require("axios");
+var cheerio = require("cheerio");
+
+// Require all models
+var db = require("./models");
+
+var PORT = 3000;
+var app = express();
+app.engine("handlebars", exphbs({
+  defaultLayout: "main"
+}));
+app.set("view engine", "handlebars");
+
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.static("public"));
+
+// Connect to the Mongo DB
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
+
+// Routes
+app.get('/', function (req, res) {
+  db.Article.find({})
+    .then(function (data) {
+      var hbsObject = {
+        Articles: data
+      };
+
+      res.render("index", hbsObject);
+    });
+});
+
+
+app.get("/scrape", function (req, res) {
+
+  axios.get("https://www.smashingmagazine.com/").then(function (response) {
+    var $ = cheerio.load(response.data);
+
+    $("h2").each(function (i, element) {
+      var result = {};
+      result.title = $(this)
+        .children("a")
+        .text();
+      result.link = "https://www.smashingmagazine.com" + $(this).children("a").attr("href");
+
+      db.Article.create(result)
+        .then(function (dbArticle) {})
+        .catch(function (err) {
+          return res.json(err);
+        });
+    });
+    res.send("Scrape Complete");
+  });
+});
+
+
+app.get("/articles", function (req, res) {
+
+  db.Article.find({})
+    .then(function (dbArticle) {
+      res.json(dbArticle);
+    })
+    .catch(function (err) {
+      res.json(err);
+    });
+});
+
+app.get("/articles/:id", function (req, res) {
+  db.Article.findOne({
+      _id: req.params.id
+    })
+    .populate("note")
+    .then(function (dbArticle) {
+      res.json(dbArticle);
+    })
+    .catch(function (err) {
+      res.json(err);
+    });
+});
+
+
+app.post("/articles/:id", function (req, res) {
+
+  db.Note.create(req.body)
+    .then(function (dbNote) {
+      return db.Article.findOneAndUpdate({
+        _id: req.params.id
+      }, {
+        note: dbNote._id
+      }, {
+        new: true
+      });
+    })
+    .then(function (dbArticle) {})
+    .catch(function (err) {
+      res.json(err);
+    });
+});
+
+app.listen(PORT, function () {
+  console.log("App running on port " + PORT + "!");
+});
