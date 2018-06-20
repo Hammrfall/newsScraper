c4d9fb1c42953e43eff02844e958797d60aa0508
Hammrfall <34660593+Hammrfall@users.noreply.github.com>
2018-06-20 12:55:41 -0400

    develop initial front end

---
+++ public/assets/javascript/main.js
@@ -0,0 +1,11 @@
+$.get("/scrape", function(req,res){
+})
+    .then(function(res){ 
+        console.log(res)  
+})
+
+// $(".viewURL").click('click',function(event){
+//     console.log($(this).attr('data_link'))
+//     $("#iframe1").attr("src",$(this).attr('data_link'))
+//     $("#iframe1").
+// })
